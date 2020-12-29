718756657ab4fd2c62f918b9ad1a3fac7c270352
Al-Azif <alazif@miskatonic.io>
2020-12-28 19:58:27 -0800

    Update for new SDK

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 History Blocker
-===============
+===
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/15442009a4f04211b2bd395d87c78f0e)](https://www.codacy.com/gh/Scene-Collective/ps4-history-blocker/dashboard)
 
-# Credits
-### Exploits
-  - qwertyoruiopz
-  - Flatz
-  - CTurt
-  - Anonymous
+## Synopsis
+Blocks the browser from remembering and returning to the last opened page on start. Run again to enable/disable.
 
-### Multi FW Payload Method
-  - CelesteBlue123
+For the payloads built with the most recent firmware supported visit [this repo].
 
-### Original Payload
-  - [stooged](https://github.com/stooged/History-Blocker)
+## Notes
+- Contribute translations for the decription in `meta.json`
+- Requires being built with the [Scene Collective Payload SDK]
+- Supports any firmware that the SDK supports
+- Originally created by [stooged]
+
+[//]: #
+  [Scene Collective Payload SDK]: <https://github.com/Scene-Collective/ps4-payload-sdk>
+  [this repo]: <https://github.com/Scene-Collective/ps4-payload-repo>
+  [stooged]: <https://github.com/stooged/History-Blocker>
--- source/main.c
+++ source/main.c
@@ -1,32 +1,33 @@
-#include "kernel_utils.h"
+#include "ps4.h"
 
 int _main(struct thread *td) {
+  UNUSED(td);
   initKernel();
   initLibc();
-  initPthread();
 
-  uint64_t fw_version = get_fw_version();
-  jailbreak(fw_version);
+  jailbreak();
 
   initSysUtil();
 
   char hfile[256];
-  SceUserServiceLoginUserIdList userIdList = getUserIDList();
+  SceUserServiceLoginUserIdList userIdList;
 
-  for (int i = 0; i < SCE_USER_SERVICE_MAX_LOGIN_USERS; i++) {
-    if (userIdList.userId[i] != -1 && userIdList.userId[i] != 0) {
-      sprintf(hfile, "/user/home/%x/webbrowser/endhistory.txt", userIdList.userId[i]);
-      if (!file_exists(hfile) && !dir_exists(hfile)) {
-        mkdir(hfile, 0777);
-        printf_notification("History blocker enabled for: %s\n\nRun payload again to disable", getUserName(userIdList.userId[i]));
-      } else {
-        if (dir_exists(hfile)) {
-          rmdir(hfile);
-          printf_notification("History blocker disabled for: %s\n\nRun payload again to enable", getUserName(userIdList.userId[i]));
-        } else if (file_exists(hfile)) {
-          unlink(hfile);
+  if (getUserIDList(&userIdList) == 0) {
+    for (int i = 0; i < SCE_USER_SERVICE_MAX_LOGIN_USERS; i++) {
+      if (userIdList.userId[i] != -1 && userIdList.userId[i] != 0) {
+        sprintf(hfile, "/user/home/%x/webbrowser/endhistory.txt", userIdList.userId[i]);
+        if (!file_exists(hfile) && !dir_exists(hfile)) {
           mkdir(hfile, 0777);
           printf_notification("History blocker enabled for: %s\n\nRun payload again to disable", getUserName(userIdList.userId[i]));
+        } else {
+          if (dir_exists(hfile)) {
+            rmdir(hfile);
+            printf_notification("History blocker disabled for: %s\n\nRun payload again to enable", getUserName(userIdList.userId[i]));
+          } else if (file_exists(hfile)) {
+            unlink(hfile);
+            mkdir(hfile, 0777);
+            printf_notification("History blocker enabled for: %s\n\nRun payload again to disable", getUserName(userIdList.userId[i]));
+          }
         }
       }
     }
